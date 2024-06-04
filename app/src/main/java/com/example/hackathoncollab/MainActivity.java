25521de7b0d2905ee427cbf9e681a95b93b40e73
Jian165 <118362738+Jian165@users.noreply.github.com>
2024-06-04 11:35:00 +0800

    commit 2

--- app/src/main/java/com/example/hackathoncollab/MainActivity.java
+++ app/src/main/java/com/example/hackathoncollab/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.hackathoncollab;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,11 +23,20 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        edt_Email =  findViewById(R.id.txt_Email);
-        edt_Password =  findViewById(R.id.txt_Password);
-        btn_loginLocalMail = findViewById(R.id.btn_Login);
+        edt_Email =  findViewById(R.id.txt_R_Email);
+        edt_Password =  findViewById(R.id.txt_R_Password);
+        btn_loginLocalMail = findViewById(R.id.btn_Register);
         btn_Gmail =  findViewById(R.id.btn_LogintWithGoogle);
         txt_CreateAccount = findViewById(R.id.txt_CreateAccount);
+
+        txt_CreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ResiterActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
 
 
 
--- app/src/main/java/com/example/hackathoncollab/ResiterActivity.java
+++ app/src/main/java/com/example/hackathoncollab/ResiterActivity.java
@@ -1,14 +1,36 @@
 package com.example.hackathoncollab;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ResiterActivity extends AppCompatActivity {
+
+    EditText UserName, Password, Email, ConfirmPassword, BackToLogin;
+    Button Register;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+    String emailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+
+    ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +38,63 @@ public class ResiterActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_resiter);
 
+        UserName =  findViewById(R.id.txt_R_UserName);
+        Password =  findViewById(R.id.txt_R_Password);
+        Email =  findViewById(R.id.txt_R_Email);
+        ConfirmPassword = findViewById(R.id.txt_R_PasswordConfirm);
+
+        Register =  findViewById(R.id.btn_Register);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PerformAuthentic();
+            }
+        });
 
 
+
+    }
+
+  private void PerformAuthentic() {
+      String email = Email.getText().toString();
+      String password = Password.getText().toString();
+      String confirmPassword = ConfirmPassword.getText().toString();
+
+      if (!email.matches(emailPattern)) {
+          Email.setError("Enter Correct Email");
+      } else if (password.isEmpty() || password.length() < 6) {
+          Password.setError("Enter Proper Password");
+      } else if (!password.equals(confirmPassword)) {
+          ConfirmPassword.setError("Password not match");
+      } else {
+          progressDialog.setMessage("Please Wait While Registration");
+          progressDialog.setTitle("Registration");
+          progressDialog.setCanceledOnTouchOutside(false);
+          progressDialog.show();
+
+          mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+              @Override
+              public void onComplete(@NonNull Task<AuthResult> task) {
+                  if (task.isSuccessful()) {
+                      progressDialog.dismiss();
+                      Toast.makeText(ResiterActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+
+                  } else {
+                      progressDialog.dismiss();
+                      Toast.makeText(ResiterActivity.this, "" + task.getException(), Toast.LENGTH_SHORT).show();
+                  }
+              }
+          });
+      }
+  }
+  private void sendUserToNextActivity() {
+        Intent intent = new Intent(ResiterActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
