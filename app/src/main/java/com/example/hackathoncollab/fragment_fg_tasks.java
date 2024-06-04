5df7f183675da42019009eabaabea7dc0d2e363d
Jian165 <118362738+Jian165@users.noreply.github.com>
2024-06-04 13:51:41 +0800

    login Done

--- app/src/main/java/com/example/hackathoncollab/fragment_fg_tasks.java
+++ app/src/main/java/com/example/hackathoncollab/fragment_fg_tasks.java
@@ -14,7 +14,10 @@ public class fragment_fg_tasks extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fg_tasks, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_fg_tasks, container, false);
+
+        return view;
+
     }
 }
